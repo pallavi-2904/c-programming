e5cc8ecc3c49926dc4dca88194232f6b76c30c47
pallavi-2904 <siyyadhripallavi03@gmail.com>
2026-04-25 12:23:57 +0530

    Add files via upload

---
+++ youanddateprogramm.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int you,date;
+    printf("Enter the stylishness of you and date:\n");
+    scanf("%d%d",&you,&date);
+    if(you<=2||date<=2)
+    {
+        printf("no=0");
+    }
+    else if(you>=8||date>=8)
+    {
+        printf("yes=2");
+    }
+    else
+    {
+        printf("maybe=1");
+    }
+
+    return 0;
+}
